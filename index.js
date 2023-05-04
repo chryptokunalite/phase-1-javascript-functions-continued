70ce72f6345103e36beec3c67d5733afd8f9fd6c
Allan Saenz <allan.d.saenz@gmail.com>
2023-05-04 12:42:15 -0400

    done

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate"){
+    return `This Saturday, I want to ${activity}!`
+}
+
+const mondayWork = function (duties = "go to the office"){
+    return `This Monday, I will ${duties}.`
+}
+
+function wrapAdjective(param1 = "*"){
+    return function(param2 = "special"){
+        return `You are ${param1}${param2}${param1}!`
+    }
+}
+wrapAdjective("%")("a dedicated programmer");
